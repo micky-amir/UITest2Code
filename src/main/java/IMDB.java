eee4c38b71ed3197d28e980670d6d54eee4897ba
amir ben ami <ambenami@microsoft.com>
2021-08-10 21:27:47 +0300

    first commit

---
+++ src/main/java/IMDB.java
@@ -0,0 +1,9 @@
+public class IMDB {
+
+    /**
+     *
+     */
+    public void test1(){
+
+    }
+}
---
+++ src/test/java/websites/IMDB.java
@@ -0,0 +1,14 @@
+package websites;
+
+import org.testng.annotations.Test;
+
+
+public class IMDB {
+    /**
+     * SK_1
+     */
+    @Test
+    public void test1(){
+
+    }
+}
