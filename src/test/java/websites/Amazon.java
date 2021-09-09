807a3d8077d28a7ba77176b0acb381f0d0b4f968
Tamar <tamar.gur@outlook.co.il>
2021-09-09 15:35:55 +0300

    Amazon SK_6-8

--- src/test/java/websites/Amazon.java
+++ src/test/java/websites/Amazon.java
@@ -136,6 +136,86 @@ public class Amazon {
         Actions actions = new Actions(driver);
         actions.moveToElement(element).click(element).perform();
         JavascriptExecutor executor = driver;
-        assertEquals(0, (long)executor.executeScript("return window.pageYOffset;"));
+        assertEquals(0, (long) executor.executeScript("return window.pageYOffset;"));
+    }
+
+    /**
+     * SK_6
+     * Tamar
+     * HTML refer to SK_1, SK_6
+     */
+    @Test
+    public void SK_6_Tamar() {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveToElement(element).perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-icp")).getCssValue("display"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='English - EN']/.."))).click();
+
+        driver.findElement(By.id("nav-cart")).click();
+        assertEquals("Your Amazon Cart is empty", driver.findElement(By.tagName("h2")).getText());
+        assertTrue(driver.findElement(By.cssSelector(".sc-sign-in .a-button-text")).getText().contains("Sign in"));
+        assertTrue(driver.findElement(By.cssSelector(".sc-sign-in .a-button:nth-child(2) a")).getText().contains("Sign up"));
+    }
+
+    /**
+     * SK_7
+     * Tamar
+     * HTML refer to SK_1, SK_7
+     */
+    @Test
+    public void SK_7_Tamar() {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveToElement(element).perform();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='English - EN']/.."))).click();
+
+        element = driver.findElement(By.id("main-content"));
+        actions.moveToElement(element).perform();
+        element.findElement(By.cssSelector(".as-title-block-right > a")).click();
+        List<WebElement> results = driver.findElements(By.cssSelector(".apb-browse-searchresults-product"));
+        for (WebElement element : results) {
+            assertTrue(element.findElement(By.tagName("img")).isDisplayed());
+            assertTrue(element.findElement(By.tagName("h2")).isDisplayed());
+            assertTrue(element.findElement(By.cssSelector(".a-icon-star-small")).isDisplayed());
+            assertTrue(element.findElement(By.className("a-price")).isDisplayed());
+            assertEquals("$", element.findElement(By.className("a-price-symbol")).getText());
+        }
+    }
+
+    /**
+     * SK_8
+     * Tamar
+     * HTML refer to SK_1, SK_8
+     */
+    @Test
+    public void SK_8_Tamar() {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveToElement(element).perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-icp")).getCssValue("display"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='English - EN']/.."))).click();
+
+        driver.findElement(By.id("searchDropdownBox")).click();
+        driver.findElement(By.xpath("//option[text()='Arts & Crafts']")).click();
+        element = driver.findElement(By.id("twotabsearchtextbox"));
+        element.sendKeys("color");
+        element.submit();
+        assertTrue(driver.findElement(By.cssSelector("#departments ul .a-text-bold")).getText().contains("Arts & Crafts"));
+
+        String searchWord = driver.findElement(By.cssSelector(".a-color-state.a-text-bold")).getText();
+        assertEquals("color", searchWord.replaceAll("\"", ""));
+
+        List<WebElement> searchResults = driver.findElements(By.xpath
+                ("//*[contains(@data-cel-widget, 'MAIN-SEARCH_RESULTS')]/descendant::a/span[contains(@class, 'a-text-normal')]"));
+        for (WebElement result : searchResults) {
+            assertTrue(result.getText().toLowerCase(Locale.ROOT).contains("color"));
+        }
     }
 }
