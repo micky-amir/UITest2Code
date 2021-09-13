fb807014932ce524b7a5ffe34934c8d93c6f4232
Tamar <tamar.gur@outlook.co.il>
2021-09-13 16:47:16 +0300

    Amazon SK_12-18

--- src/test/java/websites/Amazon.java
+++ src/test/java/websites/Amazon.java
@@ -12,8 +12,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -293,5 +292,228 @@ public class Amazon {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Amazon Music HD']"))).click();
         element = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#katana-unrec-5 h1")));
         assertTrue(element.getText().contains("AMAZON MUSIC HD"));
+    }
+
+    /**
+     * SK_12
+     * Tamar
+     * HTML refer to SK_1, SK_12
+     */
+    @Test
+    public void SK_12_Tamar() {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveByOffset(0, 0).moveToElement(element).perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-icp")).getCssValue("display"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='English - EN']/.."))).click();
+
+        driver.findElement(By.id("nav-orders")).click();
+        assertEquals("Sign-In", driver.findElement(By.tagName("h1")).getText());
+    }
+
+    /**
+     * SK_13
+     * Tamar
+     * HTML refer to SK_1, SK_13
+     */
+    @Test
+    public void SK_13_Tamar() {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveByOffset(0, 0).moveToElement(element).perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-icp")).getCssValue("display"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='English - EN']/.."))).click();
+
+        element = driver.findElement(By.id("nav-link-accountList"));
+        actions.moveToElement(element).perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-accountList")).getCssValue("display"));
+        assertTrue(driver.findElement(By.id("nav-flyout-ya-signin")).isDisplayed());
+        List<WebElement> titlesElements = driver.findElements(By.className("nav-title"));
+        assertEquals("Your Lists", titlesElements.get(0).getText());
+        assertEquals("Your Account", titlesElements.get(1).getText());
+        for (WebElement element : titlesElements) {
+            assertTrue(element.findElements(By.xpath(".//../a")).size() > 1);
+        }
+
+        driver.findElement(By.xpath("//*[text()='Create a List']/..")).click();
+        assertEquals("Lists", driver.findElement(By.className("al-intro-banner-header")).getText());
+        assertEquals("Sign In", driver.findElement(By.className("a-button-text")).getText());
+    }
+
+    /**
+     * SK_14
+     * Tamar
+     * HTML refer to SK_1, SK_12, SK_14
+     */
+    @Test
+    public void SK_14_Tamar() {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveByOffset(0, 0).moveToElement(element).perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-icp")).getCssValue("display"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='English - EN']/.."))).click();
+
+        element = driver.findElement(By.id("nav-link-accountList"));
+        actions.moveToElement(element).perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-accountList")).getCssValue("display"));
+        assertTrue(driver.findElement(By.id("nav-flyout-ya-signin")).isDisplayed());
+        List<WebElement> titlesElements = driver.findElements(By.className("nav-title"));
+        assertEquals("Your Lists", titlesElements.get(0).getText());
+        assertEquals("Your Account", titlesElements.get(1).getText());
+        for (WebElement element : titlesElements) {
+            assertTrue(element.findElements(By.xpath(".//../a")).size() > 1);
+        }
+
+        driver.findElement(By.xpath("//*[text()='Account']/..")).click();
+        assertEquals("Your Account", driver.findElement(By.tagName("h1")).getText());
+        List<WebElement> cardElements = driver.findElements(By.xpath("//h2[contains(@class, 'a-text-normal')]"));
+        List<String> expectedTitles = new LinkedList<>(Arrays.asList("Your Orders", "Login & security", "Prime",
+                "Gift cards", "Your Payments", "Your Profiles", "Your devices and content"));
+        List<String> actualTitles = new LinkedList<>();
+        for (WebElement element : cardElements) {
+            actualTitles.add(element.getText());
+        }
+        assertEquals(expectedTitles, actualTitles);
+        cardElements.clear();
+        cardElements = driver.findElements(By.cssSelector(".a-box.ya-card h2"));
+        expectedTitles.clear();
+        expectedTitles.addAll(Arrays.asList("Ordering and shopping preferences", "Digital content and devices",
+                "Memberships and subscriptions", "Communication and content", "Shopping programs and rentals",
+                "Other programs"));
+        actualTitles.clear();
+        for (WebElement element : cardElements) {
+            actualTitles.add(element.getText());
+        }
+        assertEquals(expectedTitles, actualTitles);
+
+        driver.findElement(By.xpath("//h2[contains(text(),'Your Profiles')]")).click();
+        assertEquals("Sign-In", driver.findElement(By.tagName("h1")).getText());
+    }
+
+    /**
+     * SK_15
+     * Tamar
+     * HTML refer to SK_1, SK_4, SK_12, SK_15
+     */
+    @Test
+    public void SK_15_Tamar() {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveByOffset(0, 0).moveToElement(element).perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-icp")).getCssValue("display"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='English - EN']/.."))).click();
+
+        element = driver.findElement(By.id("twotabsearchtextbox"));
+        element.sendKeys("watch");
+        element.submit();
+
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[data-cel-widget='search_result_4'] a.a-text-normal")));
+        String itemName = element.getText();
+        element.click();
+        assertEquals(itemName, driver.findElement(By.id("productTitle")).getText());
+        driver.findElement(By.id("buy-now-button")).click();
+        assertEquals("Sign-In", driver.findElement(By.tagName("h1")).getText());
+    }
+
+    /**
+     * SK_16
+     * Tamar
+     * HTML refer to SK_1, SK_4, SK_15, SK_16
+     */
+    @Test
+    public void SK_16_Tamar() {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveByOffset(0, 0).moveToElement(element).perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-icp")).getCssValue("display"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='English - EN']/.."))).click();
+
+        element = driver.findElement(By.id("twotabsearchtextbox"));
+        element.sendKeys("watch");
+        element.submit();
+
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[data-cel-widget='search_result_4'] a.a-text-normal")));
+        String itemName = element.getText();
+        element.click();
+        assertEquals(itemName, driver.findElement(By.id("productTitle")).getText());
+        driver.findElement(By.id("add-to-cart-button")).click();
+        driver.findElement(By.id("nav-cart")).click();
+
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".sc-product-link .a-truncate-cut")));
+        assertTrue(itemName.contains(element.getText()) || element.getText().contains(itemName));
+    }
+
+    /**
+     * SK_17
+     * Tamar
+     * HTML refer to SK_1, SK_4, SK_12, SK_15
+     */
+    @Test
+    public void SK_17_Tamar() {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveByOffset(0, 0).moveToElement(element).perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-icp")).getCssValue("display"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='English - EN']/.."))).click();
+
+        element = driver.findElement(By.id("twotabsearchtextbox"));
+        element.sendKeys("watch");
+        element.submit();
+
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[data-cel-widget='search_result_4'] a.a-text-normal")));
+        String itemName = element.getText();
+        element.click();
+        assertEquals(itemName, driver.findElement(By.id("productTitle")).getText());
+        driver.findElement(By.cssSelector("a[title='Add to List']")).click();
+        assertEquals("Sign-In", driver.findElement(By.tagName("h1")).getText());
+    }
+
+    /**
+     * SK_18
+     * Tamar
+     * HTML refer to SK_1, SK_18
+     */
+    @Test
+    public void SK_18_Tamar() throws InterruptedException {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveByOffset(0, 0).moveToElement(element).build().perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-icp")).getCssValue("display"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='English - EN']/.."))).click();
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("icp-nav-flyout")));
+        actions.moveByOffset(0, 0).moveToElement(element).build().perform();
+        assertNotEquals("none", driver.findElement(By.id("nav-flyout-icp")).getCssValue("display"));
+        Thread.sleep(1000);
+        driver.findElement(By.className("icp-flyout-change")).click();
+
+        assertEquals("Language Settings", driver.findElement(By.id("lop-heading")).getText());
+        assertEquals("Currency Settings", driver.findElement(By.id("icp-sc-heading")).getText());
+        driver.findElement(By.cssSelector(".a-button-dropdown")).click();
+        assertNotEquals("none", driver.findElement(By.id("a-popover-3")).getCssValue("display"));
+        driver.findElement(By.xpath("//a[contains(text(), 'ILS')]")).click();
+        driver.findElement(By.className("a-button-input")).click();
+
+        element = driver.findElement(By.id("twotabsearchtextbox"));
+        element.sendKeys("watch");
+        element.submit();
+        List<WebElement> prices = driver.findElements(By.className("a-price-symbol"));
+        for (WebElement element : prices) {
+            if(!driver.findElement(By.xpath(".//ancestor::li")).isDisplayed())
+                assertEquals("ILS", element.getText());
+        }
     }
 }
