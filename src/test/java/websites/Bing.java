21231e61b6fa4b61b7d4aad88e573ad25cf3d747
Tamar <tamar.gur@outlook.co.il>
2021-10-31 16:46:18 +0200

    Bing08

--- src/test/java/websites/Bing.java
+++ src/test/java/websites/Bing.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -102,5 +103,34 @@ public class Bing {
             assertEquals(i == 0, driver.findElement(By.cssSelector(".hp_body"))
                     .getAttribute("class").contains("no_image"));
         }
+    }
+
+    /**
+     * BING08
+     * Tamar
+     * HTML refers to BING01, BING08
+     */
+    @Test
+    public void BING08_Tamar() {
+        driver.get("https://www.bing.com");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".id_avatar")));
+        By locator = By.cssSelector("a.idp_ham");
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+        wait.until(ExpectedConditions.attributeToBe(locator, "aria-expanded", "true"));
+        element = driver.findElement(By.id("hbsettings"));
+        assertTrue(element.isDisplayed());
+        element.click();
+        wait.until(ExpectedConditions.attributeToBe(By.id("hbsettree"), "aria-hidden", "false"));
+        element = driver.findElement(By.cssSelector("#hbsettree > a[role='menuitem']"));
+        assertTrue(element.isDisplayed());
+        element.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@for='setlang']")));
+        driver.findElement(By.xpath("//*[contains(text(), 'Français')]//preceding-sibling::a")).click();
+        wait.until(ExpectedConditions.textToBe(By.id("id_s"), "Connexion"));
+        driver.navigate().back();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@for='setlang']")));
+        driver.findElement(By.xpath("//*[contains(text(), 'English')]//preceding-sibling::a")).click();
+        wait.until(ExpectedConditions.textToBe(By.id("id_s"), "Sign in"));
     }
 }
