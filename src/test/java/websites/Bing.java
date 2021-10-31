93a9a41f184d72c70d6cc21337c51539c2b42a7e
Tamar <tamar.gur@outlook.co.il>
2021-10-31 13:49:40 +0200

    Bing01, 02, 04

---
+++ src/test/java/websites/Bing.java
@@ -0,0 +1,106 @@
+package websites;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.*;
+
+public class Bing {
+    ChromeDriver driver;
+    WebElement element;
+
+    @Before
+    public void initDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        // Quit the driver
+        driver.quit();
+    }
+
+    /**
+     * BING01
+     * Tamar
+     */
+    @Test
+    public void BING01_Tamar() {
+        driver.get("https://www.bing.com");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.attributeToBe(By.cssSelector("h1 > *"), "aria-label", "Bing"));
+        assertTrue(driver.findElement(By.id("images")).isDisplayed());
+        assertTrue(driver.findElement(By.id("video")).isDisplayed());
+    }
+
+    /**
+     * BING02
+     * Tamar
+     * HTML refers to BING01
+     */
+    @Test
+    public void BING02_Tamar() {
+        driver.get("https://www.bing.com");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.attributeToBe(By.cssSelector("h1 > *"), "aria-label", "Bing"));
+        assertTrue(driver.findElement(By.id("images")).isDisplayed());
+        assertTrue(driver.findElement(By.id("video")).isDisplayed());
+        for (int i = 0; i < 2; i++) {
+            By locator = By.cssSelector("[aria-label='Settings and quick links']");
+            driver.findElement(locator).click();
+            wait.until(ExpectedConditions.attributeToBe(locator, "aria-expanded", "true"));
+            assertTrue(driver.findElement(By.xpath("//*[text()='Customize your homepage']")).isDisplayed());
+            assertTrue(driver.findElement(By.xpath("//*[text()='Show menu bar']")).isDisplayed());
+            locator = By.xpath("//*[text()='Show menu bar']//following-sibling::*");
+            assertTrue(driver.findElement(locator).getAttribute("class").contains("toggle_ctrl"));
+            driver.findElement(locator).click();
+            assertEquals("problem in" + i, i != 0, driver.findElement(By.xpath("//*[text()='Show menu bar']//following-sibling::*//*"))
+                    .getAttribute("class").contains("toggle_on"));
+            locator = By.cssSelector("[aria-label='Settings and quick links']");
+            driver.findElement(locator).click();
+            wait.until(ExpectedConditions.attributeToBe(locator, "aria-expanded", "false"));
+            assertEquals(i, driver.findElements(By.id("images")).size());
+            assertTrue(driver.findElements(By.id("video")).size() >= i);
+        }
+    }
+
+    /**
+     * BING04
+     * Tamar
+     * HTML refers to BING04
+     */
+    @Test
+    public void BING04_Tamar() {
+        driver.get("https://www.bing.com");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.attributeToBe(By.cssSelector("h1 > *"), "aria-label", "Bing"));
+        assertTrue(driver.findElement(By.id("images")).isDisplayed());
+        assertTrue(driver.findElement(By.id("video")).isDisplayed());
+        for (int i = 0; i < 2; i++) {
+            By locator = By.cssSelector("[aria-label='Settings and quick links']");
+            driver.findElement(locator).click();
+            wait.until(ExpectedConditions.attributeToBe(locator, "aria-expanded", "true"));
+            assertTrue(driver.findElement(By.xpath("//*[text()='Customize your homepage']")).isDisplayed());
+            assertTrue(driver.findElement(By.xpath("//*[text()='Show homepage image']")).isDisplayed());
+            locator = By.xpath("//*[text()='Show homepage image']//following-sibling::*");
+            assertTrue(driver.findElement(locator).getAttribute("class").contains("toggle_ctrl"));
+            driver.findElement(locator).click();
+            assertEquals("problem in" + i, i != 0, driver.findElement(By.xpath("//*[text()='Show homepage image']//following-sibling::*//*"))
+                    .getAttribute("class").contains("toggle_on"));
+            locator = By.cssSelector("[aria-label='Settings and quick links']");
+            driver.findElement(locator).click();
+            wait.until(ExpectedConditions.attributeToBe(locator, "aria-expanded", "false"));
+            assertEquals(i == 0, driver.findElement(By.cssSelector(".hp_body"))
+                    .getAttribute("class").contains("no_image"));
+        }
+    }
+}
